18096d01b12ed85bd7e926da822ac86c6a8cdc33
Luan Zekiri <luan.00@hotmail.de>
2025-04-08 01:12:06 +0200

    Show posts from database

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios"; // Ensure you have axios installed
 import "./Home.css";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import PostCard from "../Post/PostCard";
 
 /*This is the home menu where the navbar, the sidebar(to the left)
 and the main content is shown */
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/posts")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="home-container">
       <Navbar />
@@ -16,9 +30,19 @@ const Home = () => {
           <Sidebar />
         </div>
         <div className="main-content">
-        <Button variant="light">Popular</Button>
-        <Button variant="light">New</Button>
-        <PostCard />
+          <Button variant="light">Popular</Button>
+          <Button variant="light">New</Button>
+          {data.length > 0 ? (
+            data.map((post) => (
+              <PostCard
+                key={post.id}
+                createdAt={post.createdAt}
+                title={post.title}
+              />
+            ))
+          ) : (
+            <p>No posts available</p>
+          )}{" "}
         </div>
       </div>
     </div>
--- frontend/src/components/Post/PostCard.jsx
+++ frontend/src/components/Post/PostCard.jsx
@@ -3,14 +3,14 @@ import "./PostCard.css";
 import photo from '../../Assets/mock-photo.jpg';
 
 /*This is the post card where the post details are shown for the user */
-const PostCard = () => {
+const PostCard = ({content, createdAt, title, comments}) => {
   const testMethod1 = () => alert("test1")
   const testMethod2 = () => alert("test2")
 
   return (
     <div className="post-card">
       <div className="post-header">
-        <h3 className="post-title">This is the Post Title</h3>
+        <h3 className="post-title">{title}</h3>
         <p className="post-meta">
           Posted by <strong>Username</strong> in <strong>r/Subreddit</strong> - 2 hours ago
         </p>
@@ -23,7 +23,7 @@ const PostCard = () => {
           className="post-image"
         />
         <div className="post-info">
-          <p className="post-date">2 hours ago</p>
+          <p className="post-date">{createdAt}</p>
           <p className="post-comments">42 Comments</p>
           <div className="post-upvotes">
             <span className="upvote-count">1,232</span>
